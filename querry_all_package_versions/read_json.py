85e81692ccc0f63e7050b8c209d2f5b3d5b1fd5e
j.belina <j.belina@fz-juelich.de>
2025-10-02 10:10:33 +0200

    Add utility function to querry package versions

---
+++ querry_all_package_versions/read_json.py
@@ -0,0 +1,19 @@
+import json
+import pathlib
+
+# mamba search pandas --json > "C:\Programming\glaes\pandas_version.json" 2>&1
+current_directory = pathlib.Path(__file__).parent
+with open(current_directory.joinpath(r"scipy_version.json")) as handle:
+    version_dict = json.loads(handle.read())
+
+package_list = version_dict["result"]["pkgs"]
+output_version_list = []
+for current_package_dict in package_list:
+    current_version = current_package_dict["version"]
+    output_version_list.append(current_version)
+
+
+unique_version_list = sorted(list(set(output_version_list)))
+print(unique_version_list)
+
+pass
