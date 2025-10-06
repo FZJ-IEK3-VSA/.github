65a8762a2dda6b060b79868ab714394f2fe704a2
j.belina <j.belina@fz-juelich.de>
2025-10-06 16:39:18 +0200

    Increase timeoutt

---
+++ querry_all_package_versions/get_yaml_positions.py
@@ -0,0 +1,13 @@
+import yaml
+
+path_to_yaml_file = r"C:\Programming\geokit\requirements-dev.yml"
+with open(path_to_yaml_file) as stream:
+    try:
+        output_dict = yaml.safe_load(stream)
+        dependency_list = output_dict["dependencies"]
+        dependency_iterator = 0
+        for current_dep in dependency_list:
+            print(dependency_iterator, current_dep)
+            dependency_iterator = dependency_iterator + 1
+    except yaml.YAMLError as exc:
+        print(exc)
